59049332de6ac34d519bd417c265bf7c1bb3c34d
Violett-M <violetta.malachowski@gmail.com>
2024-04-25 23:36:57 -0500

    adding task

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.TaskDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@Component
+@RequestMapping("/task")
+public class TaskController {
+
+    @GetMapping("/create")
+    public String createTask(Model model){
+
+        model.addAttribute("task", new TaskDTO());
+
+        return "/task/create";
+    }
+
+}
